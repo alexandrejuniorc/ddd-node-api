df98697bb17c4382b4e632c84b8650aa217f1223
Alexandre Junior <alejunior.dev@gmail.com>
2025-05-24 12:45:05 -0300

    chore: config eslint

--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,20 +1,18 @@
-import { AnswerQuestionUseCase } from "./answer-question";
-import { AnswersRepository } from "../repositories/answers-repository";
-import { Answer } from "../entities/answer";
+import { AnswerQuestionUseCase } from './answer-question'
+import { AnswersRepository } from '../repositories/answers-repository'
+import { Answer } from '../entities/answer'
 
 const fakeAnswersRepository: AnswersRepository = {
-  create: async function (answer: Answer) {
-    return;
-  },
-};
+  create: async function (answer: Answer) {},
+}
 
-test("create an answer", async () => {
-  const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepository);
+test('create an answer', async () => {
+  const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepository)
   const answer = await answerQuestion.execute({
-    instructorId: "instructor-1",
-    questionId: "question-1",
-    content: "This is an answer",
-  });
+    instructorId: 'instructor-1',
+    questionId: 'question-1',
+    content: 'This is an answer',
+  })
 
-  expect(answer.content).toEqual("This is an answer");
-});
+  expect(answer.content).toEqual('This is an answer')
+})
