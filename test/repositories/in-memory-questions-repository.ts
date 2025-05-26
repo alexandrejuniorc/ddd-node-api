0c66ca596491935b883e58cd50dc8bc9e51ca6a7
Alexandre Junior <alejunior.dev@gmail.com>
2025-05-26 17:35:23 -0300

    feat: implements use case to fetch a list of recent questions

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -1,3 +1,4 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
 import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
 import { Question } from '@/domain/forum/enterprise/entities/question'
 
@@ -8,7 +9,7 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
     this.items.push(question)
   }
 
-  async save(question: Question): Promise<void> {
+  async save(question: Question) {
     const questionIndex = this.items.findIndex(
       (item) => item.id === question.id,
     )
@@ -30,7 +31,7 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
     return question
   }
 
-  async findById(questionId: string): Promise<Question | null> {
+  async findById(questionId: string) {
     const question = this.items.find(
       (item) => item.id.toString() === questionId,
     )
@@ -40,6 +41,14 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
     }
 
     return question
+  }
+
+  async findManyRecent({ page }: PaginationParams) {
+    const questions = this.items
+      .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
+      .slice((page - 1) * 20, page * 20)
+
+    return questions
   }
 
   async delete(question: Question): Promise<void> {
