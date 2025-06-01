3f73b4482f5277937d4bf1acc85464ecc1c65804
Alexandre Junior <alejunior.dev@gmail.com>
2025-06-01 16:47:02 -0300

    feat: create use case to read a notification

--- test/repositories/in-memory-notifications-repository.ts
+++ test/repositories/in-memory-notifications-repository.ts
@@ -6,6 +6,28 @@ export class InMemoryNotificationsRepository
 {
   public items: Notification[] = []
 
+  async findById(notificationId: string) {
+    const notification = this.items.find(
+      (item) => item.id.toString() === notificationId,
+    )
+
+    if (!notification) {
+      return null
+    }
+
+    return notification
+  }
+
+  async save(notification: Notification) {
+    const index = this.items.findIndex(
+      (item) => item.id.toString() === notification.id.toString(),
+    )
+
+    if (index >= 0) {
+      this.items[index] = notification
+    }
+  }
+
   async create(notification: Notification) {
     this.items.push(notification)
   }
