15687b256880062dc5af79b0ed02e4883efb6057
Alexandre Junior <alejunior.dev@gmail.com>
2025-06-02 00:03:01 -0300

    feat: create flow to use sub in best answer

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -1,3 +1,4 @@
+import { DomainEvents } from '@/core/events/domain-events'
 import { PaginationParams } from '@/core/repositories/pagination-params'
 import { QuestionAttachmentsRepository } from '@/domain/forum/application/repositories/question-attachments-repository'
 import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
@@ -12,6 +13,8 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
 
   async create(question: Question) {
     this.items.push(question)
+
+    DomainEvents.dispatchEventsForAggregate(question.id)
   }
 
   async save(question: Question) {
@@ -21,6 +24,8 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
 
     if (questionIndex >= 0) {
       this.items[questionIndex] = question
+
+      DomainEvents.dispatchEventsForAggregate(question.id)
     } else {
       this.items.push(question)
     }
